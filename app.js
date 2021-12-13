2f592e8f070cb8fa5649a65a54985c171bc81bcb
rikin459 <4pater48@solent.ac.uk>
2021-12-13 16:04:26 +0000

    making base of the database

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const express = require("express");
- 
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path =require('path'); 
+
 const app = express()
 
-app.get('/', (req,res)=>{
-    res.send("Crud Application");
+dotenv.config ({path:'config.env'})
+const PORT = process.env.PORT||8080
+
+// log requests
+app.use(morgan('tiny'));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended: true}))
+
+//set view engine
+app.set("view engine","ejs")
+//app.set("views",path.resolve(__dirname,"views/ejs"))
+
+//load design
+app.use('/css',express.static(path.resolve(__dirname,"FE/css")))
+app.use('/img',express.static(path.resolve(__dirname,"FE/img")))
+app.use('/javascript',express.static(path.resolve(__dirname,"FE/javascript")))
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
 })
 
-app.listen(3000,()=>{console.log('Server is running on http://localhost:3000')});
+app.listen(PORT,()=>{console.log('Server is running on http://localhost:PORT')});
