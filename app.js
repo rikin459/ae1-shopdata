8f4a5ba8c83ac70167f12f5f554118872e5613aa
rikin459 <4pater48@solent.ac.uk>
2022-01-11 13:01:12 +0000

    trying to make the CRUD

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path =require('path'); 
+const process = require("process");
+const mongoose = require("mongoose");
+
 
 const app = express()
 
 const { PORT, MONGODB_URI } = process.env;
+
 
 // log requests
 app.use(morgan('tiny'));
@@ -20,9 +24,39 @@ app.set("view engine","ejs")
 
 //load design
 app.use('/css',express.static(path.resolve(__dirname,"main/css")))
-app.use('/img',express.static(path.resolve(__dirname,"main/img")))
+app.use('/background',express.static(path.resolve(__dirname,"main/background")))
 app.use('/javascript',express.static(path.resolve(__dirname,"main/javascript")))
 
-app.use('/',require('./server/routes/router'))
+
+app.get("/", (req, res) => {
+  res.render("index")
+});
+
+app.get("/addlogs", (req, res) => {
+  res.render('addlogs', { errors: {} })
+});
+
+app.get("/updatelogs", (req, res) => {
+  res.render('updatelogs', { errors: {} })
+});
+
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+mongoose.connection.on("error", (err) => {
+
+    console.error(err);
+
+    console.log(
+
+        "MongoDB connection error. Please make sure MongoDB is running.",
+
+        chalk.red("✗")
+
+    );
+
+    process.exit();
+
+});
 
 app.listen(PORT,()=>{console.log('Server is running on http://localhost:PORT')});
